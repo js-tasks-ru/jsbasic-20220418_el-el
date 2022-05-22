7328fdb297939264dd7ba9c8e7dc493fb00d5014
Елена <el_el_@mail.ru>
2022-05-22 15:42:02 +0300

    Скрыть элемент по нажатию кнопки

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,11 @@
 function toggleText() {
-  // ваш код...
+  const btn = document.querySelector('.toggle-text-button');
+  const text = document.querySelector('#text');
+  btn.onclick = function() {
+    if (text.hidden) {
+      text.hidden = false;
+    } else {
+      text.hidden = true;
+    }
+  };
 }
