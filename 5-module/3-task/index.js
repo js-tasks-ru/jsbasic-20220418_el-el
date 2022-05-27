736410a39ada58192679200c9abdabcf67b80f83
Елена <el_el_@mail.ru>
2022-05-27 15:14:43 +0300

    Учебный проект (пролог): Карусель

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,41 @@
 function initCarousel() {
-  // ваш код...
+  const btnPrev = document.querySelector('.carousel__arrow_left');
+  const btnNext = document.querySelector('.carousel__arrow_right');
+  const container = document.querySelector('.carousel__inner');
+  let width = container.offsetWidth;
+  let count = 0;
+  btnPrev.style.display = 'none';
+
+  btnNext.onclick = function () {
+    if (count < 3) {
+      btnPrev.style.display = '';
+      count = count + 1;
+      container.style.transform = `translateX(-${width * count}px)`;
+      if (count == 3) {
+        btnNext.style.display = 'none';
+      }
+    }
+  };
+
+  btnPrev.onclick = function () {
+    if (count < 4) {
+      btnNext.style.display = '';
+      btnPrev.style.display = '';
+      count = count - 1;
+      container.style.transform = `translateX(-${width * count}px)`;
+      if (count == 0) {
+        btnPrev.style.display = 'none';
+      }
+    }
+  };
 }
+
+
+
+
+
+
+
+
+
+
